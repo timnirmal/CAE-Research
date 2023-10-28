11038506bef9d2d5d737f67f5f1c7758e3ad8e2f
Thimira Nirmal <timnirmal@gmail.com>
2023-10-28 17:45:11 +0530

    create classes

--- model_definitions/cae.py
+++ model_definitions/cae.py
@@ -3,24 +3,30 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Input
 from tensorflow.keras.models import Model
 
 class ConvolutionalAutoencoder:
-    def __init__(self, config):
-        self.config = config
+    def __init__(self, input_shape=(28, 28, 1), encoder_filters=[32, 64], decoder_filters=[64, 32],
+                 kernel_size=(3, 3), pooling_size=(2, 2), upsampling_size=(2, 2)):
+        self.input_shape = input_shape
+        self.encoder_filters = encoder_filters
+        self.decoder_filters = decoder_filters
+        self.kernel_size = kernel_size
+        self.pooling_size = pooling_size
+        self.upsampling_size = upsampling_size
         self.autoencoder = self.build_autoencoder()
 
     def build_autoencoder(self):
-        input_img = Input(shape=self.config['input_shape'])
+        input_img = Input(shape=self.input_shape)
         x = input_img
-        for i, filters in enumerate(self.config['encoder_filters']):
-            x = Conv2D(filters, self.config['kernel_size'], activation='relu', padding='same')(x)
-            x = MaxPooling2D(self.config['pooling_size'], padding='same')(x)
+        for filters in self.encoder_filters:
+            x = Conv2D(filters, self.kernel_size, activation='relu', padding='same')(x)
+            x = MaxPooling2D(self.pooling_size, padding='same')(x)
 
         encoded = x
 
-        for i, filters in enumerate(reversed(self.config['decoder_filters'])):
-            x = Conv2D(filters, self.config['kernel_size'], activation='relu', padding='same')(x)
-            x = UpSampling2D(self.config['upsampling_size'])(x)
+        for filters in reversed(self.decoder_filters):
+            x = Conv2D(filters, self.kernel_size, activation='relu', padding='same')(x)
+            x = UpSampling2D(self.upsampling_size)(x)
 
-        decoded = Conv2D(self.config['input_shape'][-1], self.config['kernel_size'], activation='sigmoid', padding='same')(x)
+        decoded = Conv2D(self.input_shape[-1], self.kernel_size, activation='sigmoid', padding='same')(x)
 
         autoencoder = Model(input_img, decoded)
         return autoencoder
@@ -32,7 +38,6 @@ class ConvolutionalAutoencoder:
         self.autoencoder.fit(x_train, x_train, epochs=epochs, batch_size=batch_size, shuffle=True, validation_data=(x_val, x_val))
 
 # Usage:
-# cae_config = {...}
 # cae = ConvolutionalAutoencoder(cae_config)
 # cae.compile()
 # cae.fit(x_train, x_test)
--- model_definitions/cnn.py
+++ model_definitions/cnn.py
@@ -1,22 +1,28 @@
 from tensorflow.keras.layers import Flatten, Dense
 
 class ConvolutionalNeuralNetwork:
-    def __init__(self, config):
-        self.config = config
+    def __init__(self, input_shape=(28, 28, 1), conv_filters=[32, 64], kernel_size=(3, 3),
+                 pooling_size=(2, 2), dense_units=[64], num_classes=10):
+        self.input_shape = input_shape
+        self.conv_filters = conv_filters
+        self.kernel_size = kernel_size
+        self.pooling_size = pooling_size
+        self.dense_units = dense_units
+        self.num_classes = num_classes
         self.cnn_model = self.build_cnn()
 
     def build_cnn(self):
-        input_img = Input(shape=self.config['input_shape'])
+        input_img = Input(shape=self.input_shape)
         x = input_img
-        for filters in self.config['conv_filters']:
-            x = Conv2D(filters, self.config['kernel_size'], activation='relu')(x)
-            x = MaxPooling2D(self.config['pooling_size'])(x)
+        for filters in self.conv_filters:
+            x = Conv2D(filters, self.kernel_size, activation='relu')(x)
+            x = MaxPooling2D(self.pooling_size)(x)
 
         x = Flatten()(x)
-        for units in self.config['dense_units']:
+        for units in self.dense_units:
             x = Dense(units, activation='relu')(x)
 
-        output = Dense(self.config['num_classes'], activation='softmax')(x)
+        output = Dense(self.num_classes, activation='softmax')(x)
 
         cnn_model = Model(input_img, output)
         return cnn_model
@@ -27,8 +33,8 @@ class ConvolutionalNeuralNetwork:
     def fit(self, x_train, y_train, x_val, y_val, epochs=10, batch_size=64):
         self.cnn_model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(x_val, y_val))
 
+
 # Usage:
-# cnn_config = {...}
 # cnn = ConvolutionalNeuralNetwork(cnn_config)
 # cnn.compile()
 # cnn.fit(x_train, y_train, x_test, y_test)
